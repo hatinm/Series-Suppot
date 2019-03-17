37482e77661330252ecdd965105dd3b3daec8ca9
hatinm <48410878+hatinm@users.noreply.github.com>
2019-03-17 18:02:18 +0300

    Update Series Support .py

--- Series Support .py
+++ Series Support .py
@@ -16,7 +16,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith("b!modcall"):
+    if message.content.startswith("-support"):
         em = discord.Embed(title = "Call!", description='The following person has requested support \n Please note to ask them about what happened and proof of that!**If they fake called mute them for 10 minutes!**`To claim this call do -claim  and to end the call run -end`\n User who called:'+message.author.mention, footer = 'Burgerizzr Call System 2019 <&/532985783229874236>', colour = 0x00d6e5 )
         await client.send_message(client.get_channel('552655874544369664'), 'CALL!', embed=em)
         await client.send_message(message.author, "I have called support for you, please provide proof to the moderator answering your call! **If you fake called, You will be muted for 10 minutes!**")
